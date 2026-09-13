fab1a71381c3a873d62b3c17e120d67399833393
DeniskaGod <pilich.den@mail.ru>
2026-09-13 19:43:51 +0300

    feat: implement notification system with dropdown and API integration
    - Added Notification API to fetch notifications from the server.
    - Created NotificationList and NotificationItem components to display notifications.
    - Integrated NotificationButton in the Navbar for accessing notifications.
    - Implemented AvatarDropdown for user profile actions in the Navbar.
    - Styled notification components with SCSS for better UI.
    - Added polling for notifications to keep the list updated.
    - Created temporary scripts for testing notification skeletons and observing their behavior.

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -7,13 +7,11 @@ import { classNames } from "@/shared/lib/classNames/classNames";
 import { Text, TextTheme } from "@/shared/ui/Text/Text";
 import { AppLink, AppLinkTheme } from "@/shared/ui/AppLink/AppLink";
 import { RoutePath } from "@/shared/config/routeConfig/routeConfig";
-import Avatar from "@/shared/ui/Avatar/Avatar";
 import Button, { ThemeButton } from "@/shared/ui/Button/Button";
 import LoginModal from "@/features/AuthByUsername/ui/LoginModal/LoginModal";
 import { HStack } from "@/shared/ui/Stack";
-import { Icon } from "@/shared/ui/Icon/Icon";
-import NotificationIcon from "@/shared/assets/icons/notification-20-20.svg";
-import { Dropdown } from "@/shared/ui/Popups";
+import { NotificationButton } from "@/features/notificationButton";
+import { AvatarDropdown } from "@/features/avatarDropdown";
 interface NavbarProps {
   className?: string;
 }
@@ -59,26 +57,8 @@ export const Navbar = memo(({ className }: NavbarProps) => {
           {t("Создать статью")}
         </AppLink>
         <HStack gap="16" className={cls.actions}>
-          <Button theme={ThemeButton.CLEAR}>
-            <Icon Svg={NotificationIcon} inverted />
-          </Button>
-          <Dropdown
-            direction="bottom left"
-            items={[
-              ...(isAdminPanelVisible
-                ? [{ content: t("Админ панель"), href: RoutePath.admin_panel }]
-                : []),
-              {
-                content: t("Профиль"),
-                href: RoutePath.profile + authData.id,
-              },
-              {
-                content: t("Выйти"),
-                onClick: onLogout,
-              },
-            ]}
-            trigger={<Avatar size={30} src={authData.avatar} />}
-          />
+          <NotificationButton />
+          <AvatarDropdown />
         </HStack>
       </header>
     );
